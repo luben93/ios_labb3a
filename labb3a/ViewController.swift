84617169bbb9a07a42146dcb3a34b1c241631456
lucas persson <luben93@gmail.com>
2017-01-05 00:21:27 +0100

    init and simple core motion work

--- labb3a/ViewController.swift
+++ labb3a/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    let manager = CMMotionManager()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        startAcc()
+    }
+    
+    func startAcc(){
+        if manager.isAccelerometerAvailable {
+            //print(1)
+            manager.accelerometerUpdateInterval = 0.01
+            manager.startAccelerometerUpdates(to: OperationQueue.main) {
+                [weak self] (data: CMAccelerometerData?, error: Error?) in
+                //print(2)
+                if let acceleration = data?.acceleration {
+                    //print(3)
+                    let rotation = atan2(acceleration.x, acceleration.y) - M_PI
+                    
+                    let label = self?.degreeLabel
+                    label?.transform = CGAffineTransform(rotationAngle: CGFloat(rotation))
+                    label?.text = "\(rotation)°"
+                }
+            }
+                }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var degreeLabel: UILabel!
 
 }
 
