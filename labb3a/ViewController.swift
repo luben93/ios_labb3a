8ebdf86dbf04d9de07604fb748884b3d2f9626e2
lucas persson <luben93@gmail.com>
2017-01-04 23:19:13 +0100

    Initial Commit

---
+++ labb3a/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  labb3a
+//
+//  Created by lucas persson on 2017-01-04.
+//  Copyright © 2017 lucas persson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
