d68ccbd05c6c2f0ef1954f9a196a52d703783284
lucas persson <luben93@gmail.com>
2017-01-05 17:20:24 +0100

    not working, started on 1

--- labb3a/ViewController.swift
+++ labb3a/ViewController.swift
@@ -12,7 +12,30 @@ import CoreMotion
 class ViewController: UIViewController {
 
     let manager = CMMotionManager()
+    @IBOutlet weak var magnetLabel: UILabel!
+    @IBOutlet weak var degreeLabel: UILabel!
+    @IBOutlet weak var gyroLabel: UILabel!
+    var F = 0.90
+    var magnet = 0.0{
+        didSet{
+            magnetLabel.text = magnet.description
+        }
+    }
+    var gyro = 0.0{
+        didSet{
+            gyroLabel.text = magnet.description
+        }
+    }
 
+    var acceler = 0.0{
+        didSet{
+            degreeLabel.text = magnet.description
+        }
+    }
+
+    private func filter(newval:Double, lastval:Double){
+        lastval = F * lastval + (1-F) * newval
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,20 +48,38 @@ class ViewController: UIViewController {
     func startAcc(){
         if manager.isAccelerometerAvailable {
             //print(1)
-            manager.accelerometerUpdateInterval = 0.01
+            manager.accelerometerUpdateInterval = 0.1
             manager.startAccelerometerUpdates(to: OperationQueue.main) {
                 [weak self] (data: CMAccelerometerData?, error: Error?) in
                 //print(2)
                 if let acceleration = data?.acceleration {
-                    //print(3)
-                    let rotation = atan2(acceleration.x, acceleration.y) - M_PI
-                    
-                    let label = self?.degreeLabel
-                    label?.transform = CGAffineTransform(rotationAngle: CGFloat(rotation))
-                    label?.text = "\(rotation)°"
+                    self?.acceler = acceleration.z
                 }
             }
+        }
+        if manager.isGyroAvailable {
+            //print(1)
+            manager.gyroUpdateInterval = 0.1
+            manager.startGyroUpdates(to: OperationQueue.main) {
+                [weak self] (data: CMGyroData?, error: Error?) in
+                //print(2)
+                if let rate = data?.rotationRate {
+                    self?.gyro = rate.z
                 }
+            }
+        }
+        if manager.isMagnetometerAvailable {
+            //print(1)
+            manager.magnetometerUpdateInterval = 0.1
+            manager.startMagnetometerUpdates(to: OperationQueue.main) {
+                [weak self] (data: CMMagnetometerData?, error: Error?) in
+                //print(2)
+                if let rate = data?.magneticField {
+                    self?.magnet = rate.z
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,7 +87,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBOutlet weak var degreeLabel: UILabel!
 
 }
 
